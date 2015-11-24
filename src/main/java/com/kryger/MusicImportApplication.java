290b32e34eefa43ed818d3378c2f2eb42bd70804
Lukasz Kryger <lukasz.kryger@ncr.com>
2015-11-24 21:31:19 +0000

    Barebones project from start.spring.io

---
+++ src/main/java/com/kryger/MusicImportApplication.java
@@ -0,0 +1,12 @@
+package com.kryger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicImportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicImportApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kryger/MusicImportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kryger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MusicImportApplication.class)
+public class MusicImportApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
